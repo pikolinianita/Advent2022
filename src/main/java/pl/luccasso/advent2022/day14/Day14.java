f9c3433b6912629a55b7cd011268e14cb0a861f7
pikolinian <pikolinian@gazeta.pl>
2022-12-30 20:48:12 +0100

    d14 works one or other, not both

--- src/main/java/pl/luccasso/advent2022/day14/Day14.java
+++ src/main/java/pl/luccasso/advent2022/day14/Day14.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.function.Function;
@@ -24,23 +25,26 @@ record Tile(int x, int y) {
 
 }
 
-final class Cave{
-    
+final class Cave {
+
     Set<Tile> cave;
 
     int originalSize;
-    
+
+    int maxDepth;
+
     public Cave(Scanner scanner) {
         cave = createCave(scanner);
         originalSize = cave.size();
+        maxDepth = cave.stream().mapToInt(Tile::y).max().getAsInt();
     }
-    
+
     final Set<Tile> createCave(Scanner scanner) {
         var result = scanner.useDelimiter("\n")
                 .tokens()
                 .map(line -> Arrays.stream(line.split(" -> ")).map(this::processLine).toList())
                 .map(this::fillLine)
-                .flatMap(s->s.stream())
+                .flatMap(s -> s.stream())
                 .collect(Collectors.toSet());
         return result;
     }
@@ -50,25 +54,25 @@ final class Cave{
         if ("->".equals(line)) {
             return null;
         } else {
-          var pair = line.split(",");
-          return new Tile(Integer.parseInt(pair[0]), Integer.parseInt(pair[1]));
+            var pair = line.split(",");
+            return new Tile(Integer.parseInt(pair[0]), Integer.parseInt(pair[1]));
         }
     }
 
-    List<Tile> fillLine(List<Tile> list){
+    List<Tile> fillLine(List<Tile> list) {
         var result = new LinkedList<Tile>();
         var iter = list.iterator();
         var previous = iter.next();
         Tile next;
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             next = iter.next();
             Tile vector;
-            if(previous.x()==next.x()){
-                vector = previous.y()-next.y()<0 ? new Tile(0, 1): new Tile (0, -1);
-            } else{
-                vector = previous.x()-next.x()<0 ? new Tile(1,0) : new Tile (-1, 0);
+            if (previous.x() == next.x()) {
+                vector = previous.y() - next.y() < 0 ? new Tile(0, 1) : new Tile(0, -1);
+            } else {
+                vector = previous.x() - next.x() < 0 ? new Tile(1, 0) : new Tile(-1, 0);
             }
-                
+
             result.addAll(fillFragment(previous, next, vector));
             previous = next;
         }
@@ -79,41 +83,50 @@ final class Cave{
         var result = new ArrayList<Tile>();
         result.add(previous);
         Tile current = previous;
-        do{
+        do {
             current = new Tile(current.x() + vector.x(), current.y() + vector.y());
             result.add(current);
         } while (!current.equals(next));
         return result;
     }
-    
-    public boolean exist(Tile tile){
+
+    public boolean exist(Tile tile) {
         return cave.contains(tile);
     }
-    
-    public Cave add(Tile tile){
+
+    public boolean exist(int x, int y) {
+        return cave.contains(new Tile(x, y));
+    }
+
+    public Cave add(Tile tile) {
         cave.add(tile);
         return this;
     }
-    
-    public int size(){
+
+    public int size() {
         return cave.size();
     }
-    
-    public int getSand(){
-        return originalSize - cave.size();
+
+    public int getSand() {
+        return cave.size() - originalSize;
     }
-    
+
+    public int getMaxDeepth() {
+        return maxDepth;
+    }
+
 }
 
 public class Day14 {
 
-  
     Cave cave;
     private final Tile start;
+    private boolean finishedP1;
 
     public Day14(Cave cave) {
         this.cave = cave;
-        start = new Tile (500,0);
+        start = new Tile(500, 0);
+        finishedP1 = false;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -122,8 +135,58 @@ public class Day14 {
     }
 
     private void calculate() {
-        
+       // System.out.println("Answer part 1 is: " + getP1Answer());
+        System.out.println("Answer part 2 is: " + getP2Answer());
     }
 
+    int getP1Answer() {
+        drop(start);
+        return cave.getSand();
+    }
     
+    int getP2Answer() {
+        dropP2(start);
+        return cave.getSand();
+    }
+
+    void drop(Tile start) {
+        if (start.y() > cave.maxDepth || finishedP1) {
+            finishedP1 = true;
+            return;
+        }
+        if (!cave.exist(start.x(), start.y() + 1)) {
+            drop(new Tile(start.x(), start.y() + 1));
+        }
+        if (!cave.exist(start.x() - 1, start.y() + 1)) {
+            drop(new Tile(start.x() - 1, start.y() + 1));
+        }
+        if (!cave.exist(start.x() + 1, start.y() + 1)) {
+            drop(new Tile(start.x() + 1, start.y() + 1));
+        }
+
+        if (!finishedP1) {
+            cave.add(start);
+        }
+    }
+
+    void dropP2(Tile start) {
+        
+        if (start.y() > cave.maxDepth + 1) {
+            // finishedP2 = true;
+            return;
+        }
+        if (!cave.exist(start.x(), start.y() + 1)) {
+            dropP2(new Tile(start.x(), start.y() + 1));
+        }
+        if (!cave.exist(start.x() - 1, start.y() + 1)) {
+            dropP2(new Tile(start.x() - 1, start.y() + 1));
+        }
+        if (!cave.exist(start.x() + 1, start.y() + 1)) {
+            dropP2(new Tile(start.x() + 1, start.y() + 1));
+        }
+
+        cave.add(start);
+       
+    }
+
 }
--- src/test/java/pl/luccasso/advent2022/day14/Day14Test.java
+++ src/test/java/pl/luccasso/advent2022/day14/Day14Test.java
@@ -17,29 +17,68 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author piko
  */
 public class Day14Test {
-    
+
     String testInput = """
                        498,4 -> 498,6 -> 496,6
                        503,4 -> 502,4 -> 502,9 -> 494,9""";
-    
+
     public Day14Test() {
-    }  
-    
+    }
+
     @Test
     public void fillLine() {
-       
-        var cave = new Cave(new Scanner(""));
-        System.out.println(cave.fillLine(List.of(new Tile(10,10), new Tile (10,20))));
-        System.out.println(cave.fillLine(List.of(new Tile(10,10), new Tile (20,10))));
-        System.out.println(cave.fillLine(List.of(new Tile(10,10), new Tile (10,0))));
-        System.out.println(cave.fillLine(List.of(new Tile(10,10), new Tile (0,10))));
-        
+
+        var cave = new Cave(new Scanner(testInput));
+        System.out.println(cave.fillLine(List.of(new Tile(10, 10), new Tile(10, 20))));
+        System.out.println(cave.fillLine(List.of(new Tile(10, 10), new Tile(20, 10))));
+        System.out.println(cave.fillLine(List.of(new Tile(10, 10), new Tile(10, 0))));
+        System.out.println(cave.fillLine(List.of(new Tile(10, 10), new Tile(0, 10))));
+
     }
-    
+
     @Test
     public void caveBuildTest() {
         var cave = new Cave(new Scanner(testInput));
         assertThat(cave.size()).isEqualTo(20);
-        
+
     }
+
+    @Test
+    public void caveSolveTest() {
+        var cave = new Cave(new Scanner(testInput));
+        var day = new Day14(cave);
+        var result = day.getP1Answer();
+        assertThat(result).isEqualTo(24);
+
+    }
+
+    @Test
+    public void caveSolveTestp2() {
+        var cave = new Cave(new Scanner(testInput));
+        var day = new Day14(cave);
+        var result = day.getP2Answer();
+        assertThat(result).isEqualTo(93);
+    }
+
+    @Test
+    public void caveSolveTestBoth() {
+        var cave = new Cave(new Scanner(testInput));
+        var day = new Day14(cave);
+        var result1 = day.getP1Answer();
+        var result2 = day.getP2Answer();
+        assertThat(result1).isEqualTo(24);
+        assertThat(result2).isEqualTo(93);
+    }
+    
+    @Test
+    public void caveSolveTestBothReversed() {
+        var cave = new Cave(new Scanner(testInput));
+        var day = new Day14(cave);
+        var result2 = day.getP2Answer();
+        var result1 = day.getP1Answer();        
+        
+        assertThat(result2).isEqualTo(93);
+        assertThat(result1).isEqualTo(24);
+    }
+
 }
